6a245d497b618284bd4a214e6f68d896dab4bffb
Darryl <darrylmangibin@gmail.com>
2019-03-22 13:02:07 +0800

    WOODS and BRANDS model added

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_I = 10;
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
 const userSchema = mongoose.Schema({
   email: {
@@ -13,8 +17,8 @@ const userSchema = mongoose.Schema({
     minlength: 5
   },
   name: {
-    type: String,
     required: true,
+    type: String,
     maxlength: 100
   },
   lastname: {
@@ -24,10 +28,10 @@ const userSchema = mongoose.Schema({
   },
   cart: {
     type: Array,
-    default: []
+    default: [],
   },
   history: {
-    type: Array, 
+    type: Array,
     default: []
   },
   role: {
@@ -38,6 +42,51 @@ const userSchema = mongoose.Schema({
     type: String
   }
 });
+
+userSchema.pre('save', function(next){
+  var user = this;
+
+  if(user.isModified('password')) {
+    bcrypt.genSalt(SALT_I, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next()
+  }
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), process.env.SECRET)
+  user.token = token;
+  user.save(function(err, user) {
+    if(err) return cb(err);
+    cb(null, user);
+  })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this;
+  
+  jwt.verify(token, process.env.SECRET, function(err, decode) {
+    user.findOne({"_id":decode,"token":token}, function(err, user) {
+      if(err) return cb(err);
+      cb(null, user);
+    });
+  })
+}
 
 const User = mongoose.model('User', userSchema);
 
--- server/server.js
+++ server/server.js
@@ -3,27 +3,127 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE);
 
+// register middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-// Models
+// MODELS
 const { User } = require('./models/user');
+const { Brand } = require('./models/brand');
+const { Wood } = require('./models/wood');
 
-//=============================================
-//                   USERS
-//=============================================
+// MIDDLEWARES
+const { auth } = require('./middleware/auth');
+const { admin } = require('./middleware/admin');
+
+// ===========================================
+//                  BRAND
+// ===========================================
+app.post('/api/product/brand',auth, admin, (req, res) => {
+  const brand = new Brand(req.body);
+  brand.save((err, doc) => {
+    if(err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      brand: doc
+    })
+  })
+})
+
+app.get('/api/product/brands', (req, res) => {
+  Brand.find({}, (err, brands) => {
+    if(err) return res.status(400).send(err);
+    res.status(400).send(brands)
+  })
+})
+
+// ===========================================
+//                  WOODS
+// ===========================================
+app.post('/api/product/wood', auth, admin, (req, res) => {
+  const wood = new Wood(req.body);
+  wood.save((err, doc) => {
+    if(err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      wood: doc
+    })
+  })
+})
+
+app.get('/api/product/woods', (req, res) => {
+  Wood.find({}, (err, woods) => {
+    if(err) return res.status(400).send(err);
+    res.status(200).send(woods)
+  })
+})
+
+// ===========================================
+//                  USERS
+// ===========================================
+app.get('/api/users/auth', auth, (req, res) => {
+  res.status(200).json({
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email:req. user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    cart: req.user.cart,
+    history: req.user.history
+  })
+})
+
+// LOGOUT
+app.get('/api/user/logout', auth, (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { token: '' },
+    (err, doc) => {
+      if(err) return res.json({ success: false, err })
+      return res.status(200).send({
+        success: true
+      })
+    }
+  )
+})
+
+// REGISTER
 app.post('/api/users/register', (req, res) => {
-  res.status(200)
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if(err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+    })
+  })
 });
 
+// LOGIN
+app.post('/api/users/login', (req, res) => {
+  console.log(res)
+  User.findOne({ 'email': req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({ loginSucess: false, message: 'Auth failed, email not found' });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({ loginSucess: 'false', message: 'Wrong Password' });
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        res.cookie('w_auth', user.token).status(200).json({
+          loginSucess: true
+        })
+      })
+    })
+  })
+})
 
 const port = process.env.PORT || 3002;
 
