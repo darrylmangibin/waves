654675ef708f316acfa9db926a7adcb23a363af9
Darryl <darrylmangibin@gmail.com>
2019-03-23 18:05:55 +0800

    Header Links

--- client/src/components/Header_footer/Header/index.js
+++ client/src/components/Header_footer/Header/index.js
@@ -1,6 +1,73 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+
+  state = {
+    page: [
+      {
+        name: 'Home',
+        linkTo: '/',
+        public: true
+      },
+      {
+        name: 'Guitars',
+        linkTo: '/shop',
+        public: true
+      },
+      
+    ],
+    user:[
+      {
+        name: 'My Cart',
+        linkTo: '/user/cart',
+        public: false
+      }, {
+        name: 'My Account',
+        linkTo: '/user/dashboard',
+        public: false
+      }, {
+        name: 'Log in',
+        linkTo: '/register_login',
+        public: true
+      },
+      {
+        name: 'Log out',
+        linkTo: '/user/logout',
+        public: false
+      },
+    ]
+  }
+
+  defaultLink = (item, i) => {
+    return (
+      <Link to={item.linkTo} key={i}>
+        {item.name}
+      </Link>
+    )
+  }
+
+  showLinks = (type) => {
+    let list = [];
+    if(this.props.user.userData) {
+      type.forEach((item) => {
+        if(!this.props.user.userData.isAuth) {
+          if(item.public === true) {
+            list.push(item)
+          }
+        } else {
+          if(item.name !== 'Log in') {
+            list.push(item)
+          }
+        }
+      })
+    }
+    return list.map((item, i) => {
+      return this.defaultLink(item, i)
+    })
+  }
+
   render() {
     return (
       <header className="bck_b_light">
@@ -12,10 +79,10 @@ class Header extends React.Component {
           </div>
           <div className="right">
             <div className="top">
-              LINKS
+              {this.showLinks(this.state.user)}
             </div>
             <div className="bottom">
-              LINKS
+              {this.showLinks(this.state.page)}
             </div>
           </div>
         </div>
@@ -24,4 +91,10 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(Header);
