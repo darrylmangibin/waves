88f326684ed37d5472fe3c61cca2d9ddd098fc2d
Darryl <darrylmangibin@gmail.com>
2019-03-23 16:55:30 +0800

    Dashboard added

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -4,14 +4,17 @@ import Home from './components/Home';
 import Layout from './hoc/layout';
 import RegisterLogin from './components/Register_login';
 import Register from './components/Register_login/Register';
+import UserDashboard from './components/User/';
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        <Route path="/user/dashboard" exact component={UserDashboard} />
         <Route path="/" exact component={Home} />
         <Route path="/register_login" exact component={RegisterLogin} />
         <Route path="/register" exact component={Register} />
+        
       </Switch>
     </Layout>
   )
---
+++ client/src/hoc/user.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const links = [
+  {
+    name: 'My acount',
+    linkTo:'/user/dashboard'
+  },
+  {
+    name: 'User Information',
+    linkTo: '/user/user_profile'
+  },
+  {
+    name: 'My Cart',
+    linkTo: '/user/cart'
+  },
+]
+
+const UserLayout = (props) => {
+
+  const generateLinks = (links) => {
+    return (
+      links.map((item, i) => {
+        return (
+          <Link to={item.linkTo} key={i}>
+            {item.name}
+          </Link>
+        )
+      })
+    )
+  }
+
+  return (
+    <div className="container">
+      <div className="user_container">
+        <div className="user_left_nav">
+          <h2>My Account</h2>
+          <div className="links">
+            { generateLinks(links) }
+          </div>
+        </div>
+        <div className="user_right">
+          {props.children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UserLayout;
