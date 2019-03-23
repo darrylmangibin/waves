b044fe99eff22407c4078f1e1e852ea82b357a1f
Darryl <darrylmangibin@gmail.com>
2019-03-24 00:19:36 +0800

    [UPDATE]-Home card & buttons

--- client/src/components/utils/button.js
+++ client/src/components/utils/button.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faShoppingBag from '@fortawesome/fontawesome-free-solid/faShoppingBag';
 
 const MyButton = (props) => {
 
@@ -9,12 +11,25 @@ const MyButton = (props) => {
     switch(props.type) {
       case "default":
         template = <Link
-          className="link_default"
+          className={!props.altClass ? 'link_default' : props.altClass}
           to={props.linkTo}
           {...props.addStyles}
         >
           {props.title}
         </Link>
+      break
+      case 'bag_link':
+        template =(
+          <div className="bag_link"
+            onClick={() => {
+              props.runAction()
+            }}
+          >
+            <FontAwesomeIcon 
+              icon={faShoppingBag}
+            />
+          </div>
+        )
       break
       default:
         template = ''
--- client/src/components/utils/card.js
+++ client/src/components/utils/card.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MyButton from './button';
 
 class Card extends Component {
 
@@ -40,8 +41,26 @@ class Card extends Component {
             </div>
           ) : null
         }
-        <div>
-        
+        <div className="actions">
+          <div className="button_wrapp">
+            <MyButton 
+              type="default"
+              altClass="card_link"
+              title="View product"
+              linkTo={`/product_detail/${props._id}`}
+              addStyles={{
+                margin: '10px 0 0 0'
+              }}
+            />
+          </div>
+          <div className="button_wrapp">
+            <MyButton 
+              type="bag_link"
+              runAction={() => {
+                console.log('added to cart')
+              }}
+            />
+          </div>
         </div>
       </div>
     )
