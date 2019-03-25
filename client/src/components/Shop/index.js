0e05d05917a137c63e4e40736389d0aff3fe62bb
Darryl <darrylmangibin@gmail.com>
2019-03-25 09:49:32 +0800

    Checkbox shop component

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -2,15 +2,39 @@ import React, { Component } from 'react';
 import PageTop from '../utils/page_top';
 import { connect } from 'react-redux';
 import { getBrands, getWoods } from '../../actions/products_actions';
+import CollapseCheckbox from '../utils/collapseCheckbox';
+
+import { frets } from '../utils/Form/fixed_categories';
 
 class Shop extends Component {
+
+  state = {
+    grid:'',
+    limit: 6,
+    skip: 0,
+    filters: {
+      brand: [],
+      frets: [],
+      woods: [],
+      price: []
+    }
+  }
 
   componentDidMount() {
     this.props.dispatch(getBrands());
     this.props.dispatch(getWoods())
   }
 
+  handleFilters = (filters, category) => {
+    const newFilters = {...this.state.filters}
+    newFilters[category] = filters;
+    this.setState({
+      filters:newFilters
+    })
+  }
+
   render() {
+    console.log(this.state.filters)
     const products = this.props.products;
     return (
       <div>
@@ -20,7 +44,24 @@ class Shop extends Component {
         <div className="container">
           <div className="shop_wrapper">
             <div className="left">
-              left
+              <CollapseCheckbox 
+                initState={true}
+                title="Brands"
+                list={products.brands}
+                handleFilters={(filters) => this.handleFilters(filters, 'brand')}
+              />
+              <CollapseCheckbox 
+                initState={false}
+                title="Frets"
+                list={frets}
+                handleFilters={(filters) => this.handleFilters(filters, 'frets')}
+              />
+              <CollapseCheckbox 
+                initState={false}
+                title="Wood"
+                list={products.woods}
+                handleFilters={(filters) => this.handleFilters(filters, 'woods')}
+              />
             </div>
             <div className="right">
               right
