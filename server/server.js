3f0a18cccd070c1b9dad7bc4a96b7340ff0c8ed0
Darryl <darrylmangibin@gmail.com>
2019-03-27 01:49:20 +0800

    [UPDATE] successBuy route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const formidable = require('express-formidable');
 const cloudinary = require('cloudinary');
+const async = require('async');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -339,6 +340,45 @@ app.post('/api/users/successBuy', auth, (req, res) => {
   }
   transactionData.data = req.body.paymentData;
   transactionData.product = history;
+
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $push: {history: history}, $set:{ cart: [] } },
+    { new: true },
+    (err, user) => {
+      if(err) return res.json({ success: false, err });
+      const payment = new Payment(transactionData);
+      payment.save((err, doc) => {
+        if(err) return res.json({ success: false, err });
+        let products = [];
+        doc.product.forEach((item) => {
+          products.push({
+            id: item.id,
+            quantity: item.quantity
+          })
+        })
+        async.eachOfSeries(products, (item, callback) => {
+          Product.update(
+            {_id: item.id},
+            {$inc: {
+              "sold": item.quantity
+            }},
+            { new: false },
+            callback
+          )
+        }, (err) => {
+          if(err) return res.json({success: false, err})
+          res.status(200),json(
+            {
+              success: true, 
+              cart: user.cart,
+              cartDetail: []
+            })
+        })
+      })
+    }
+  )
+
 })
 
 const port = process.env.PORT || 5000;
