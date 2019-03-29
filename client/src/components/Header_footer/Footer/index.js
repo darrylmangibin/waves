188a1eb794ae7a9a2f9b22d72cef554153159fcb
Darryl <darrylmangibin@gmail.com>
2019-03-29 11:36:17 +0800

    server changes

--- client/src/components/Header_footer/Footer/index.js
+++ client/src/components/Header_footer/Footer/index.js
@@ -7,6 +7,7 @@ import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
 
 
 const Footer = ({ data }) => {
+  console.log(data)
   return (
     data.siteData ?
     <footer className="bck_b_dark">
